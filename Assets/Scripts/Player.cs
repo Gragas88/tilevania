cbce30a6f6633c4fa49eb2f9758dd375b403861b
Andrey Sedov <balalaika007@gmail.com>
2020-10-22 22:17:25 +0300

    Prevent double jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,31 +6,47 @@ public class Player : MonoBehaviour
 {
     Animator animator;
     Rigidbody2D rigidBody;
-    [SerializeField] float moveSpeed = 5f;
+    Collider2D collider;
+    [SerializeField] float jumpSpeed = 25f;
+    [SerializeField] float moveSpeed = 7f;
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        collider = GetComponent<Collider2D>();
     }
-
 
     void Update()
     {
         Run();
+        Jump();
     }
 
     private void Run() {
         float deltaX = Input.GetAxis("Horizontal");
-        Vector2 velocity = new Vector2(deltaX * moveSpeed, rigidBody.velocity.y);
+        var velocity = new Vector2(deltaX * moveSpeed, rigidBody.velocity.y);
         
         rigidBody.velocity = velocity;
 
         bool isRunning = Mathf.Abs(rigidBody.velocity.x) > Mathf.Epsilon;
-        
+
         animator.SetBool("Running", isRunning);
 
         if (isRunning) {
             transform.localScale = new Vector2(Mathf.Sign(rigidBody.velocity.x), 1f);
+        }
+    }
+
+    private void Jump() {
+        if (!collider.IsTouchingLayers(LayerMask.GetMask("Ground"))) {
+            return;
+        }
+
+
+        bool isJumping = Mathf.Abs(rigidBody.velocity.y) > Mathf.Epsilon;
+        
+        if (Input.GetButtonDown("Jump")) {
+            rigidBody.velocity += new Vector2(0f, jumpSpeed);
         }
     }
 }
